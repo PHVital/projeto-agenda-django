b8f5e1342b27f76f66cd94ad254fbae0ee52e041
PHVital <vital.ph.g@gmail.com>
2025-04-24 16:15:18 -0300

    Criando o search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,12 +1,41 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.http import Http404
+from django.db.models import Q
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
+    }
+
+    return render (
+        request,
+        'contact/index.html',
+        context,
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects.filter(show=True)\
+        .filter(
+            Q(first_name__contains=search_value) |  
+            Q(last_name__contains=search_value) |
+            Q(phone__contains=search_value) |  
+            Q(email__contains=search_value)   
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render (
@@ -19,8 +48,11 @@ def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
     
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
 
     return render (
