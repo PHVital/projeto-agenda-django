fb843ab63672855b7201ddddabb681bd8ff06870
PHVital <vital.ph.g@gmail.com>
2025-04-27 13:59:58 -0300

    Adicionando feature para apenas o owner poder editar e deletar usuarios

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -42,7 +42,7 @@ def create(request):
 
 @login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user)
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
@@ -76,7 +76,7 @@ def update(request, contact_id):
 
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user)
     confirmation = request.POST.get('confirmation', 'no')
 
     if confirmation == 'yes':
